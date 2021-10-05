dab0224abfeba2dc4880dd9bce308974e3d21e76
Daniel <me@danieldevine.dev>
2021-10-06 00:47:06 +0100

    Add stucture for main class, movements tests

--- helpers/legs.py
+++ helpers/legs.py
@@ -1,6 +1,7 @@
 from quadrupod.bot import Leg
 from quadrupod.utils import load_config
-from uasyncio import run
+# from uasyncio import run, gather
+import uasyncio
 
 CONFIG = load_config('../config.json')
 
@@ -23,9 +24,31 @@ def get_leg(ref):
 
 def defaults():
     async def func():
-        for l in LEGS:
-            await l.move_to_default()
+        moves = []
+        for leg in LEGS:
+            moves.append(leg.move_to_origin())
+        await uasyncio.gather(*moves)
+    uasyncio.run(func())
 
-    run(func())
+
+def test_all():
+    async def func():
+        await uasyncio.gather(*[leg.upper.move_to_default() for leg in LEGS])
+        for leg in LEGS:
+            await leg.move()
 
 
+def stand():
+    async def func():
+        await uasyncio.gather(*[leg.lower.move_to_default() for leg in LEGS])
+        mid_moves = []
+        up_moves = []
+        for leg in LEGS:
+            mid_moves.append(leg.middle.move(leg.middle.percentage_to_degrees(0.5), 1.5))
+            up_moves.append(leg.upper.move(leg.upper.percentage_to_degrees(0.2), 1.5))
+
+        await uasyncio.gather(*up_moves)
+        await uasyncio.gather(*mid_moves)
+        # await uasyncio.sleep(1.5)
+
+    uasyncio.run(func())
--- quadrupod/bot.py
+++ quadrupod/bot.py
@@ -1,43 +1,59 @@
 from quadrupod import pca9685
-from machine import I2C, Pin, SoftI2C
+from machine import Pin, SoftI2C
 import uasyncio
 
 
 class Servo:
-    SERVOS = pca9685.Servos(I2C(scl=Pin(5), sda=Pin(4)))
+    SERVOS = pca9685.Servos(SoftI2C(scl=Pin(5), sda=Pin(4)))
     WAIT_TIME = 1.5
 
-    def __init__(self, index, default_position=None):
+    def __init__(self, index, origin_position=None):
         self.index = index
-        self.default_position = default_position
-        self.position = None
+        self.__origin = origin_position
+        self.__position = None
+
+    @property
+    def origin(self):
+        return self.__origin
+
+    @property
+    def position(self):
+        return self.__position
 
     async def move(self, degrees, wait=None):
         print(f"Index: {self.index} to {degrees} degrees")
         Servo.SERVOS.position(self.index, degrees)
         if wait:
             await uasyncio.sleep(wait)
-        self.position = degrees
+        self.__position = degrees
         Servo.SERVOS.release(self.index)
 
     async def ensure_position(self):
-        await self.move(self.position, Servo.WAIT_TIME)
+        await self.move(self.__position, Servo.WAIT_TIME)
 
     async def move_to_default(self):
-        await self.move(self.default_position, Servo.WAIT_TIME)
+        await self.move(self.__origin, Servo.WAIT_TIME)
+
+    def percentage_to_degrees(self, percentage_float=0.1):
+        degree = float(percentage_float) * 180
+        return int(abs(self.origin - degree))
 
 
 class Leg:
     def __init__(self, u_idx, m_idx, l_idx, ref=None):
-        self.position_ref = ref
-        default_pos = self._default_pos()
-        self.upper = Servo(u_idx, default_pos)
-        self.middle = Servo(m_idx, default_pos)
+        self.__ref = ref
+        origin = self.get_origin()
+        self.upper = Servo(u_idx, origin)
+        self.middle = Servo(m_idx, origin)
         self.lower = Servo(l_idx, 90)
 
     @property
     def servos(self):
         return self.upper, self.middle, self.lower
+
+    @property
+    def ref(self):
+        return self.__ref
 
     async def move(self, upper_pos=None, middle_pos=None, lower_pos=None, wait=None):
         await uasyncio.gather(
@@ -46,12 +62,37 @@ class Leg:
             self.lower.move(lower_pos, wait)
         )
 
-    def _default_pos(self):
-        d = 0
-        if self.position_ref == 'lb' or self.position_ref == 'rf':
-            d = 180
-        return d
+    def get_origin(self):
+        return 180 if self.__ref in ['lb', 'rf'] else 0
 
-    async def move_to_default(self):
+    async def move_to_origin(self):
         await uasyncio.gather(*[s.move_to_default() for s in self.servos])
+
+
+# Static Movement
+class Quadruped:
+    def __init__(self, config):
+        self.__lf = None
+        self.__lb = None
+        self.__rf = None
+        self.__rb = None
+        for ref, pos in config["legs"].items():
+            setattr(self, f"__{ref}", Leg(pos["u"], pos["m"], pos["l"], ref))
+
+    @property
+    def legs(self):
+        return self.__lb, self.__lf, self.__rb, self.__rf
+
+    def step(self):
+        pass
+
+    async def stand(self):
+        await uasyncio.gather(*[leg.upper.move_to_default() for leg in self.legs])
+        moves = []
+        for leg in self.legs:
+            move = 40 if leg.get_origin() > 0 else 140
+            moves.append(leg.move(None, move))
+
+    async def sit(self):
+        await uasyncio.gather(*[leg.move_to_origin() for leg in self.legs])
 
